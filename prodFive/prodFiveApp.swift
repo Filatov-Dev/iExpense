2792d5674643c0b6c9ab68e9bbf8b143ed2abaad
Filatov_Yurii <filatov.ios.dev@gmail.com>
2021-10-20 21:46:24 +0300

    iExpense

---
+++ prodFive/prodFiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  prodFiveApp.swift
+//  prodFive
+//
+//  Created by Юрий Филатов on 13.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct prodFiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
